48eb0a673ade2380662ddb87ae9fdb67e1c09522
addebas <adrien.debas@gmail.com>
2024-09-09 17:36:03 +0200

    AD add file

---
+++ drias.R
@@ -0,0 +1,80 @@
+library(happign)
+library(sf)
+
+install.packages(c("sf", "ggplot2", "leaflet", "dplyr"))
+
+library(sf)
+library(ggplot2)
+library(leaflet)
+library(dplyr)
+library(tmap);ttm()
+library(terra)
+library(tidyverse)
+library(stars)
+
+# chargement du jeu de données ----
+
+indices_feu <-
+  read.table(
+    "D:/COURS_APT/3A/R_SIG_projet/indicesALADIN63_CNRM-CM5_24090913012904838.KEYuAuU7dD1Uudu0Od00fOx.txt",
+    sep = ";",
+    quote = "\""
+  )
+
+safran <- st_read("D:/COURS_APT/3A/R_SIG_projet/safran.gpkg")
+
+qtm(safran)
+
+# passage des coordonnées en métrique pour le calcul des dalles ----
+
+indices_feu_sf <- st_as_sf(indices_feu, coords = c("V3", "V2"), crs = 4326)
+
+indices_feu_lamb <- st_transform(indices_feu_sf, 2154)
+
+
+# jointure safran/drias ----
+
+safran_drias <- st_join(safran, indices_feu_sf)
+
+safran_drias <- safran_drias[!is.na(safran_drias$V1),]
+
+# arrondi des indicateurs à l'unité
+
+safran_drias$V12 <- round(safran_drias$V12, 0)
+safran_drias$V12 <- as.factor(safran_drias$V12)
+
+safran_drias_proche <- safran_drias[safran_drias$V5 == "H1",]
+safran_drias_moyen <- safran_drias[safran_drias$V5 == "H2",]
+safran_drias_lointain <- safran_drias[safran_drias$V5 == "H3",]
+
+
+
+
+# Représentation cartographique ----
+
+# Visualisation avec ggplot2
+ggplot() +
+  geom_sf(data = safran_drias_proche, aes(fill = V12)) +
+  scale_fill_viridis_d() +
+  theme_minimal() +
+  labs(title = "Risque incendie - Région Grand Est",
+       fill = "IFMx")
+
+
+
+# Visualisation avec tmap
+
+qtm(safran_drias_proche, fill="V12")
+qtm(safran_drias_moyen, fill="V12")
+qtm(safran_drias_lointain, fill="V12")
+
+
+# rasterize ----
+
+# rasterize based on geometry and a column named "value". Change the name of this column if necessary
+ifmx_raster <- st_rasterize(safran_drias_proche %>% dplyr::select(V12, location))
+
+# export as tiff
+write_stars(ifmx_raster, "ifmx.tif")
+
+
